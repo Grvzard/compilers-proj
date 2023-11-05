6845abf254e8675015918734992ed08577e43ece
grvzard <grvzard@outlook.com>
2023-11-05 11:52:32 +0800

    proj1: support "input file" in cmdline option

--- proj1/main.c
+++ proj1/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "scanner.h"
 #include "token.h"
@@ -29,11 +30,44 @@ void repl() {
     }
 }
 
+static char *readFile(const char *path) {
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    fseek(file, 0L, SEEK_SET);  // rewind(file);
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char *path) {
+    char *source = readFile(path);
+    lex(source);
+    free(source);
+}
+
 int main(int argc, const char *argv[]) {
     if (argc == 1) {
         repl();
     } else if (argc == 2) {
-        // compile file
+        runFile(argv[1]);
     } else {
         printf("incorrect usage");
     }
