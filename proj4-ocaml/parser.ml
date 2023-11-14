c8befa577e0d6c5fbde70f422bc4e4d8d84763cf
grvzard <grvzard@outlook.com>
2023-11-14 12:57:26 +0800

    ref(proj4-ocaml): simplfy the process

--- proj4-ocaml/parser.ml
+++ proj4-ocaml/parser.ml
@@ -61,22 +61,19 @@ type symbol =
   | Term of Token.t
   | Nonterm of Rpn.opcodes
 
-type analysis_stack =
-  { stack : symbol Stack.t
-      (* a stack with Terminal Symbols only, make it eaiser when comparing precedence *)
-  ; op_stack : Token.t Stack.t
-  }
-
-let stack_traceback stack =
-  while not (Stack.is_empty stack.stack) do
-    match Stack.pop stack.stack with
+let stack_traceback stack : unit =
+  let print_symbol = function
     | Term token_ -> print_string (Token.to_string token_)
-    | Nonterm _ -> print_string "{T}"
-  done;
-  print_string "\n";
-  while not (Stack.is_empty stack.op_stack) do
-    print_string (Token.to_string (Stack.pop stack.op_stack))
-  done
+    | Nonterm _ -> print_string "{N}"
+  in
+  List.iter print_symbol stack
+;;
+
+let rec last_op (stack : symbol list) : Token.t =
+  match stack with
+  | [] -> raise (Failure "error code: 0")
+  | Term t :: _ -> t
+  | Nonterm _ :: ss -> last_op ss
 ;;
 
 (*
@@ -89,79 +86,58 @@ let stack_traceback stack =
    NUMBER
 *)
 
-let reduce cmp_token stack =
-  while compare (Stack.top stack.op_stack, cmp_token) = Gt do
-    ignore (Stack.pop stack.op_stack);
-    let s1 = Stack.pop stack.stack in
-    match s1 with
-    | Term (Token.Number n) ->
+let rec do_reduction (cmp : Token.t) (l : symbol list) : symbol list =
+  (* stack_traceback (List.rev l); *)
+  (* print_endline ""; *)
+  if compare (last_op l, cmp) <> Gt
+  then l
+  else (
+    match l with
+    | Term (Token.Number n) :: ls ->
       let new_q = Queue.create () in
       Queue.push (Rpn.OpNumber n) new_q;
-      Stack.push (Nonterm new_q) stack.stack
-    | Term Token.RParen ->
-      let s2 = Stack.pop stack.stack in
-      let s3 = Stack.pop stack.stack in
-      (match s2, s3 with
-       | Nonterm _, Term Token.LParen ->
-         Stack.push s2 stack.stack;
-         (* pop '(' *)
-         ignore (Stack.pop stack.op_stack)
-       | _ -> raise (Failure "reduce failed"))
-    | Nonterm codes_r ->
-      let s2 = Stack.pop stack.stack in
-      let s3 = Stack.pop stack.stack in
-      (match s2, s3 with
-       | ( Term ((Token.Plus | Token.Minus | Token.Multiply | Token.Divide) as token_)
-         , Nonterm codes_l ) ->
-         Queue.transfer codes_r codes_l;
-         Stack.push s3 stack.stack;
-         (match token_ with
-          | Token.Plus -> Queue.push Rpn.OpPlus codes_l
-          | Token.Minus -> Queue.push Rpn.OpMinus codes_l
-          | Token.Multiply -> Queue.push Rpn.OpMultiply codes_l
-          | Token.Divide -> Queue.push Rpn.OpDivide codes_l
-          | _ -> assert false)
-       | _ -> raise (Failure "reduce failed"))
-    | _ -> raise (Failure "reduce failed")
-  done
+      do_reduction cmp (Nonterm new_q :: ls)
+    | Term Token.RParen :: (Nonterm _ as e) :: Term Token.LParen :: ls ->
+      do_reduction cmp (e :: ls)
+    | Nonterm codes_r :: Term t :: (Nonterm codes_l as e) :: ls ->
+      Queue.transfer codes_r codes_l;
+      (match t with
+       | Token.Plus -> Queue.push Rpn.OpPlus codes_l
+       | Token.Minus -> Queue.push Rpn.OpMinus codes_l
+       | Token.Multiply -> Queue.push Rpn.OpMultiply codes_l
+       | Token.Divide -> Queue.push Rpn.OpDivide codes_l
+       | _ -> raise (Failure "error code: 1"));
+      do_reduction cmp (e :: ls)
+    | _ -> raise (Failure "error code: 2"))
 ;;
 
-let token_is_operator = function
-  | Token.Plus
-  | Token.Minus
-  | Token.Multiply
-  | Token.Divide
-  | Token.LParen
-  | Token.RParen
-  | Token.End -> true
-  | Token.Number _ -> false
+let reduce cmp_token stack : symbol list =
+  if compare (last_op stack, cmp_token) = Gt
+  then do_reduction cmp_token stack
+  else raise (Failure "error code: 5")
 ;;
 
-(* Token.t list -> Rpn.ast (Reverse Polish Notation) *)
+(* Token.t Queue.t -> Rpn.opcodes (Reverse Polish Notation) *)
 let tokens_to_rpn tokens =
-  if Queue.length tokens < 1 then raise (Failure "too few tokens");
-  let stack = { stack = Stack.create (); op_stack = Stack.create () } in
-  Stack.push Token.End stack.op_stack;
+  Queue.push Token.End tokens;
+  if Queue.length tokens <= 1 then raise (Failure "no input");
+  let stack = Term Token.End :: [] in
   let rec inner stack tokens =
-    if Stack.length stack.op_stack > 1 || Queue.length tokens > 1
+    if Queue.peek tokens <> Token.End
     then (
-      let left_t = Stack.top stack.op_stack in
+      let left_t = last_op stack in
       let right_t = Queue.peek tokens in
       match compare (left_t, right_t) with
-      | Lt | Eq ->
-        Stack.push (Term (Queue.take tokens)) stack.stack;
-        Stack.push right_t stack.op_stack;
-        inner stack tokens
-      | Gt ->
-        reduce right_t stack;
-        inner stack tokens)
+      | Lt | Eq -> inner (Term (Queue.take tokens) :: stack) tokens
+      | Gt -> inner (reduce right_t stack) tokens)
     else (
-      match Stack.pop stack.stack with
-      | Nonterm rpn -> rpn
+      match reduce Token.End stack with
+      | [ Nonterm rpn; Term Token.End ] -> rpn
       | _ ->
         print_string "stack traceback: ";
         stack_traceback stack;
-        raise (Failure "parse failed: "))
+        print_endline "";
+        raise (Failure "error code: 6"))
   in
   inner stack tokens
 ;;
